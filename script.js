611963b88d002c0853c1f0f7f164a6f5a05d40c0
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-04-25 15:41:30 -0300

    Requisito 3 e 4 completados, acabaram sendo feitos simultâneamente não intencionalmente

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let rgbHtml = document.getElementById('rgb-color');
-function rgbGen() {
+let cores = document.querySelector('.cores');
+function rgbGen(elemento) {
     let rgb = [];
     let sorteioRgb = 0;
     for (let index = 0; index < 3; index += 1) {
         sorteioRgb = Math.floor(Math.random() * 256);
         rgb.push(sorteioRgb);
     }
-    rgbHtml.innerText ="(" + rgb + ")";
+    return elemento.innerText = "(" + rgb + ")";
 }
-rgbGen();
+function geraCores() {
+    for (let key = 0; key < 6; key += 1) {
+    let cor = document.createElement('div');
+    cor.className = 'ball';
+    let sorteioCor = '';
+    sorteioCor = rgbGen(sorteioCor);
+    cor.style.backgroundColor = 'rgb' + sorteioCor;
+    cores.appendChild(cor);
+    }
+}
+geraCores();
+rgbGen(rgbHtml);
