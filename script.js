48a372e80d1c38f6037024034b57822b9bbbebe3
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-04-25 16:48:42 -0300

    Requisito 5 completado

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let rgbHtml = document.getElementById('rgb-color');
 let cores = document.querySelector('.cores');
+let bolaCores = document.querySelector('.cores').children;
+let secaoCores = document.getElementsByTagName('section');
+function textoJogo(string) {
+    let textoDoJogo = document.createElement('p');
+    textoDoJogo.setAttribute('id', 'answer');
+    textoDoJogo.innerText = string;
+    secaoCores[0].appendChild(textoDoJogo);
+}
 function rgbGen(elemento) {
     let rgb = [];
     let sorteioRgb = 0;
@@ -9,6 +17,7 @@ function rgbGen(elemento) {
     }
     return elemento.innerText = "(" + rgb + ")";
 }
+//Gera os 6 elementos com cores aleatórias
 function geraCores() {
     for (let key = 0; key < 6; key += 1) {
     let cor = document.createElement('div');
@@ -16,8 +25,25 @@ function geraCores() {
     let sorteioCor = '';
     sorteioCor = rgbGen(sorteioCor);
     cor.style.backgroundColor = 'rgb' + sorteioCor;
+    cor.addEventListener('click', verificacao);
     cores.appendChild(cor);
     }
 }
-geraCores();
+//Atribui o elemento com ID answer e atribui a cor designada
+function resposta() {
+    let escolhaResposta = bolaCores[Math.floor(Math.random() * 6)];
+    escolhaResposta.setAttribute('id', 'resposta');
+    escolhaResposta.style.backgroundColor = 'rgb' + rgbHtml.innerHTML;
+}
+//Verificação se o clique foi no elemento com ID especificado
+function verificacao(evento) {
+    if (evento.target.id === 'resposta') {
+        secaoCores[0].lastChild.innerText = "Acertou!";
+    } else {
+        secaoCores[0].lastChild.innerText = "Errou! Tente novamente!";
+    }
+}
 rgbGen(rgbHtml);
+geraCores();
+resposta();
+textoJogo("Escolha uma cor");
